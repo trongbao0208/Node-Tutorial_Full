eda59e1a05c7db9a27fd0bb3f1290f2b1b8f25a9
Trong Bao <trongbao0208@gmail.com>
2022-08-05 21:42:53 -0500

    new commit

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,26 @@
-console.log('Express Tutorial')
+const express = require("express");
+const app = express();
+const logger = require('./logger')
+// req => middleware => res
+
+app.use(logger);
+
+
+app.get("/", (req, res) => {
+  res.send("Home");
+});
+
+app.get("/about", (req, res) => {
+  res.send("About");
+});
+
+app.get("/api/products", (req, res) => {
+    res.send("About");
+});
+
+app.get("/api/items", (req, res) => {
+    res.send("About");
+  });
+app.listen(5001, () => {
+  console.log("Server is listening on port 5001.....");
+});
